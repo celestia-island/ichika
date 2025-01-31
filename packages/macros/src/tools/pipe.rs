4c9a1e8f8413d0736dc76b1f5b9c365b86c4b716
伊欧 <langyo.china@gmail.com>
2025-01-31 23:51:50 +0800

    ✨ Support match expression.

--- packages/macros/src/tools/pipe.rs
+++ packages/macros/src/tools/pipe.rs
@@ -1,21 +1,62 @@
 use syn::{
+    braced,
     parse::{Parse, ParseStream},
-    Token,
+    Expr, Token,
 };
 
 use super::ClosureMacros;
 
 #[derive(Debug, Clone)]
+pub enum PipeNode {
+    Closure(ClosureMacros),
+    Map(Vec<MatchNode>),
+}
+
+#[derive(Debug, Clone)]
+pub struct MatchNode {
+    condition: Expr,
+    body: PipeNode,
+}
+
+#[derive(Debug, Clone)]
 pub struct PipeMacros {
-    closures: Vec<ClosureMacros>,
+    closures: Vec<PipeNode>,
 }
 
 impl Parse for PipeMacros {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let mut closures = vec![];
+
+        fn dfs(input: ParseStream) -> syn::Result<PipeNode> {
+            if input.peek(Token![|]) {
+                let closure = input.parse()?;
+                Ok(PipeNode::Closure(closure))
+            } else if input.peek(Token![match]) {
+                input.parse::<Token![match]>()?;
+                let content;
+                braced!(content in input);
+                let mut nodes = vec![];
+
+                while !content.is_empty() {
+                    let condition = content.parse()?;
+                    content.parse::<Token![=>]>()?;
+                    let body = dfs(&content)?;
+                    nodes.push(MatchNode { condition, body });
+
+                    if content.peek(Token![,]) {
+                        content.parse::<Token![,]>()?;
+                    }
+                }
+
+                Ok(PipeNode::Map(nodes))
+            } else {
+                Err(syn::Error::new(input.span(), "Expected closure or match"))
+            }
+        }
+
         while !input.is_empty() {
-            let closure = input.parse()?;
-            closures.push(closure);
+            let node = dfs(input)?;
+            closures.push(node);
 
             if input.peek(Token![,]) {
                 input.parse::<Token![,]>()?;
