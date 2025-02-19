6618362f684142cb0b4ecc8ffb1e1ddac9b0d6dd
伊欧 <langyo.china@gmail.com>
2025-02-19 14:02:59 +0800

    🧪 Rewrite some test cases.

--- packages/types/tests/pipe_async.rs
+++ packages/types/tests/pipe_async.rs
@@ -4,9 +4,29 @@ use ichika::pipe;
 
 #[test]
 fn create_async_pipe() -> Result<()> {
-    let pool = pipe![
-        async |req: String| -> usize { Ok(req.len()) },
-        async |req: usize| -> String { Ok(req.to_string()) }
+    let pool = pipe! [
+      async |(name: String, checksum: Vec<u8>, url: String)|  {
+        Ok((name, id, reqwest::get(url).await?))
+      },
+      |(name, checksum, buffer)| {
+        let mut sha3 = sha3::Sha3_256::new();
+        sha3.update(&buffer);
+        ensure!(sha3[..] == checksum, "oops!");
+        Ok((name, buffer))
+      },
+      |(name, buffer)| {
+        let mut decoder = flate2::read::GzDecoder::new();
+        let mut ret = vec![];
+        decoder.read_to_end(&mut ret)?;
+        Ok((name, data))
+      },
+      async |(name, data)| {
+        tokio::fs::write(
+          format!("./{name}.dat"),
+          &data
+        );
+        Ok(())
+      }
     ];
 
     Ok(())
--- packages/types/tests/pipe_multi.rs
+++ packages/types/tests/pipe_multi.rs
@@ -3,15 +3,38 @@ use anyhow::Result;
 use ichika::pipe;
 
 #[test]
-fn create_pipe_with_multiple_target() -> Result<()> {
+fn create_pipe_with_result_target() -> Result<()> {
     let pool = pipe![
-        |req: String| -> usize { Ok(req.len()) },
-        match {
-            0 => |req: usize| -> String { Ok(format!("from 1 {req}")) },
-            _ => match {
-                1 => |req: usize| -> String { Ok(format!("from 2 {req}")) },
-                _ => |req: usize| -> String { Ok(format!("from 3 {req}")) }
-            },
+        |req: String| -> usize {
+            if req.len() > 10 {
+                Ok(1)
+            } else {
+                Err("error")
+            }
+        },
+        catch |req: &str| -> String { Ok(req.to_string()) },
+        |req: usize| -> String { Ok(req.to_string()) }
+    ];
+
+    Ok(())
+}
+
+#[test]
+fn create_pipe_with_switchable_target() -> Result<()> {
+    let pool = pipe![
+        |req: String| -> usize {
+            if req.len() > 10 {
+                Ok(ichika::Status::Switch("target_a"))
+            } else if req.len() > 5 {
+                Ok(ichika::Status::Switch("target_b"))
+            } else {
+                Ok(ichika::Status::Switch("target_c"))
+            }
+        },
+        {
+            target_a: |req: usize| -> String { Ok(format!("a{req}")) },
+            target_b: |req: usize| -> String { Ok(format!("b {req}")) },
+            target_c: |req: usize| -> String { Ok(format!("c {req}")) }
         },
         |req: usize| -> String { Ok(req.to_string()) }
     ];
